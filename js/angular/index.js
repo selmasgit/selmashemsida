6372ab90adfe1791f33da7d3a08d8cca39f78dae
Selma Björklund <hello@selmabjorklund.com>
2017-11-21 21:37:03 +0000

    Add php and angular controls

---
+++ js/angular/index.js
@@ -0,0 +1,14 @@
+var app = angular.module('selmashemsida', [])
+
+app.controller('indexController', ['$scope', '$timeout', function($scope, $timeout, $window, $http, $filter) {
+    $scope.currentPage = 'home.html';
+    $scope.isLoading = false;
+
+    $scope.changePage = function(page)
+    {
+        $scope.isLoading = true;
+        $scope.currentPage = page;
+        $scope.isLoading = false;
+        toastrNotification.showNotification("Page changed!", 'success');
+    };
+}]);
